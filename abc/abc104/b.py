2c90b14b8c0d7f468ec9209a8629692a9585e5ec
nafuka <42476527+nafuka11@users.noreply.github.com>
2019-05-16 14:09:59 +0900

    Fix abc104 b

--- abc/abc104/b.py
+++ abc/abc104/b.py
@@ -1,10 +1,3 @@
-S = input()
-ans = True
+import re
 
-if S[0] != 'A':
-    ans = False
-elif S[2:-1].count('C') != 1:
-    ans = False
-elif not (S[1] + S[2:-1].replace('C', '') + S[-1]).islower():
-    ans = False
-print('AC' if ans else 'WA')
+print('AC' if re.match(r'^A[a-z]+C[a-z]+$', input()) else 'WA')
