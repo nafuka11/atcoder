7a6ad58785a547367c739fdbf25addcfbd3a62bd
nafuka <42476527+nafuka11@users.noreply.github.com>
2019-05-20 20:15:19 +0900

    Fix abc103 b

--- abc/abc103/b.py
+++ abc/abc103/b.py
@@ -1,8 +1,3 @@
 S = input()
 T = input()
-ans = False
-for i in range(len(S)):
-    if S[-i::] + S[:-i] == T:
-        ans = True
-        break
-print("Yes" if ans else "No")
+print("Yes" if T in S * 2 else "No")
