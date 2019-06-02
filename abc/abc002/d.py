7db2f92898cf50caca4502883261ae5ab34d3cc8
nafuka <42476527+nafuka11@users.noreply.github.com>
2019-06-02 10:54:58 +0900

    Fix abc002 d

--- abc/abc002/d.py
+++ abc/abc002/d.py
@@ -4,12 +4,6 @@ N, M = map(int, input().split())
 xy = [tuple(map(int, input().split())) for _ in range(M)]
 for n in range(N, 0, -1):
     for combi in combinations(range(1, N+1), n):
-        is_fraction = True
-        for x in range(len(combi)-1):
-            for y in range(x+1, len(combi)):
-                if (combi[x], combi[y]) not in xy:
-                    is_fraction = False
-                    break
-        if is_fraction:
+        if all([(x, y) in xy for x, y in combinations(combi, 2)]):
             print(n)
             exit()
