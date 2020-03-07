198625a3afcbbd11f28fa516a20d365fb9860c2f
nafuka <42476527+nafuka11@users.noreply.github.com>
2020-03-08 00:16:07 +0900

    Update abc158 d
    左側, 右側の文字列の末尾に文字を追加していくように

--- abc/abc158/d.cpp
+++ abc/abc158/d.cpp
@@ -1,105 +1,42 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void query(deque<char> &l, deque<char> &r, bool &reversed, bool &l_reversed, bool &r_reversed)
+void query(string &l, string &r)
 {
   int T;
   cin >> T;
   if (T == 1)
   {
-    reversed = !reversed;
     swap(l, r);
-    swap(l_reversed, r_reversed);
-    if (l.size() > 1)
-    {
-      l_reversed = !l_reversed;
-    }
-    if (r.size() > 1)
-    {
-      r_reversed = !r_reversed;
-    }
+    return;
+  }
+  int F;
+  char c;
+  cin >> F >> c;
+  if (F == 1)
+  {
+    l += c;
   }
   else
   {
-    int F;
-    cin >> F;
-    char c;
-    cin >> c;
-    if (F == 1)
-    {
-      if (l_reversed)
-      {
-        l.push_back(c);
-      }
-      else
-      {
-        l.push_front(c);
-      }
-    }
-    else
-    {
-      if (r_reversed)
-      {
-        r.push_front(c);
-      }
-      else
-      {
-        r.push_back(c);
-      }
-    }
+    r += c;
   }
 }
 
-void show_ans(deque<char> &l, deque<char> &r, string &S, bool reversed, bool l_reversed, bool r_reversed)
+void show_ans(string &l, string &r)
 {
-  if (l_reversed)
-  {
-    for (int i = 0; i < l.size(); i++)
-    {
-      cout << l.at(l.size() - i - 1);
-    }
-  }
-  else
-  {
-    for (int i = 0; i < l.size(); i++)
-    {
-      cout << l.at(i);
-    }
-  }
-  if (reversed)
-  {
-    reverse(S.begin(), S.end());
-  }
-  cout << S;
-  if (r_reversed)
-  {
-    for (int i = 0; i < r.size(); i++)
-    {
-      cout << r.at(r.size() - i - 1);
-    }
-  }
-  else
-  {
-    for (int i = 0; i < r.size(); i++)
-    {
-      cout << r.at(i);
-    }
-  }
-  cout << endl;
+  reverse(l.begin(), l.end());
+  cout << l << r << endl;
 }
 
 int main()
 {
-  string S;
+  string s_l, s_r;
   int Q;
-  cin >> S >> Q;
-  bool reversed = false;
-  bool l_reversed = false;
-  bool r_reversed = false;
-  deque<char> l, r;
+  cin >> s_r >> Q;
   for (int i = 0; i < Q; i++)
   {
-    query(l, r, reversed, l_reversed, r_reversed);
+    query(s_l, s_r);
   }
-  show_ans(l, r, S, reversed, l_reversed, r_reversed);
+  show_ans(s_l, s_r);
 }
